6b3f55dc8e8e564b0b80a8c48d37a40df8beb1e7
Comum <miguel.rib.20@gmail.com>
2019-01-03 23:08:23 +0000

    Take 4 ...

---
+++ precache-manifest.49c9a1f0bb39e7c9061c40a7609492f8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c27a966ece9db3c644bd",
+    "url": "/static/js/main.c27a966e.chunk.js"
+  },
+  {
+    "revision": "7f8e4c1b4cf8002cc0bf",
+    "url": "/static/js/1.7f8e4c1b.chunk.js"
+  },
+  {
+    "revision": "5fec0c5f6e0b9878bbad2e114f59d15d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://comum.github.io/precache-manifest.1f211154e1a900789fe4d8edcfe2dd26.js"
+  "/precache-manifest.49c9a1f0bb39e7c9061c40a7609492f8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://comum.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
